dbe720ed31c68247a19be38413336765b9473ccd
BobbieBarker <sirkemera@gmail.com>
2015-05-24 14:18:07 -0700

    stuff

--- src/app/main/user-post-list-display.js
+++ src/app/main/user-post-list-display.js
@@ -19,23 +19,23 @@ angular.module('sdkDemo.landing.user-post-list', [])
       return this.postList;
     }), function(newVal, oldVal){
       if(!_.isUndefined(vm.postList)){
-        vm.posts = pageCount(vm.postList, itemsPerPage);
+        vm.posts = pageCount(vm.postList, itemsPerPage, 1);
       }
     });
 
     vm.getComments = function(id){
       Users.getPostsComments(id).then(function(data){
-        vm.comments = pageCount(data, itemsPerPage);
+        vm.comments = pageCount(data, itemsPerPage, 0);
         console.log(vm.comments)
       })
     }
 
-    var pageCount = function(postList, perPage){
+    var pageCount = function(postList, perPage, countAt){
       var result = {};
       result.pagination = [];
       if(!_.isUndefined(postList)){
         var divisor = Math.ceil(postList.length/itemsPerPage);
-        for(var i = 1; i < divisor; i++){
+        for(var i = countAt; i < divisor; i++){
           result.pagination.push({currentPage: i});
         }
         result.list = _.chunk(postList, itemsPerPage);
